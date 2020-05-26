d279039fe15df8217e20a9a04b859be5d41288c1
snehalpatil <snehalpro55@gmail.com>
2020-05-26 14:52:25 +0530

    worked on Login Page

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import {useState,setOpen} from 'react';
+
+
 
 import '../assets/css/Login.css'
 import { Container, Row, Col} from 'react-bootstrap';
+import { Button } from 'react-bootstrap'
+import { Form , FormGroup, FormLabel, FormControl } from 'react-bootstrap'
+
 
 export default class ClickMe extends React.Component {
     constructor(props) {
@@ -13,6 +19,7 @@ export default class ClickMe extends React.Component {
        }
     }
     moveAboveLayer = () =>{
+      // setOpen(!open);
       this.setState(prevState => {
         let style= Object.assign({},prevState.style);
         style.left = "600px";
@@ -21,25 +28,57 @@ export default class ClickMe extends React.Component {
     }
     render() {
       // const fade = this.state.fade
-  
+      
       return (
           <section className="Login">
             <div className="LoginWrapeer">
               <div className="BellowLayer">
                 <Container>
                   <Row>
-                    <Col></Col>
                     <Col>
-                      <div>
-                        <h3>are you student?</h3>
-                        <button onClick={this.moveAboveLayer}>LogIn</button>
+                    {/* <Fade in={open}>
+        <div id="example-fade-text">
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+          labore wes anderson cred nesciunt sapiente ea proident.
+        </div>
+      </Fade> */}
+                    </Col>
+                    <Col xs={4}></Col>
+                    <Col>
+                      <div className="BellowLayerRightPanel">
+                        <h1>Are you <br/>student?</h1>
+                        <p>Login as a Student</p>
+                        <Button 
+                          className="ButtonStudentLogin" 
+                          // aria-controls="example-fade-text" 
+                          // aria-expanded={open} 
+                          onClick={this.moveAboveLayer}>
+                        LogIn
+                        </Button>
                       </div>
                     </Col>
                   </Row>
                 </Container>
               </div>
               <div className="AboveLayer" style={this.state.style}>
-
+                <p style={{fontSize:"40px",paddingLeft:"30px"}}>LOGIN</p>
+                <Form className="Form">
+                  <FormGroup>
+                    
+                    <FormControl 
+                      className="AboveLayerFormControl" 
+                      placeholder="Email">
+                    </FormControl>
+                  </FormGroup>
+                  <FormGroup>
+                    <FormControl
+                      className="AboveLayerFormControl"
+                      placeholder="Password"
+                    ></FormControl>
+                  </FormGroup>
+                  <Button className="formSubmitButton">Submit</Button>
+                </Form>
               </div>
             </div>
           </section>        

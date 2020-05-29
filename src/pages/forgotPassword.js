1f4ae93793483363e64a2b91092c02c21300a46d
snehalpatil <snehalpro55@gmail.com>
2020-05-29 19:46:10 +0530

    added forget password file

--- src/pages/forgotPassword.js
+++ src/pages/forgotPassword.js
@@ -1,15 +1,32 @@
 import React from "react";
 
-import {Card} from "react-bootstrap"
+import {Card, FormControl,FormLabel} from "react-bootstrap"
+
+import '../assets/css/ForgotPass.css'
 
 export default class ForgotPassword extends React.Component{
     render(){
         return(
-            <div>
-               <h1>Forgot Password</h1>
-               <Card>
-                </Card> 
+            <div className="ForgotPass">
+                <div className="ForgorPassWrapper">
+                    <Card className="ForgorPassWrapperCard">
+                        <Card.Body>
+                            <Card.Title className="ForgorPassWrapperCardTitle"><h1>Forgot Password</h1></Card.Title>
+                            <Card.Text>
+                                <FormLabel className="ForgorPassWrapperLabel">Enter Email</FormLabel>
+                                <FormControl className="ForgorPassWrapperFormControl"></FormControl>
+                                <FormLabel className="ForgorPassWrapperLabel">Enter Password</FormLabel>
+                                <FormControl className="ForgorPassWrapperFormControl"></FormControl>
+                                <FormLabel className="ForgorPassWrapperLabel">Confirm Password</FormLabel>
+                                <FormControl className="ForgorPassWrapperFormControl"></FormControl>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card> 
+                </div>
+            
             </div>
+            
+           
         );
     }
 }
